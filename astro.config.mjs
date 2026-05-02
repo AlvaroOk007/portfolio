9b32b0fe63a8de104f77ee19c3ad95c7567f58fc
Alvaro Bravo <bravoalva19@gmail.com>
2026-05-02 17:09:03 -0300

    Se agregó seccion de proyectos

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://alvarook007.github.io',
   base: '/portfolio',
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 })
---
+++ src/i18n/ui.js
@@ -0,0 +1,21 @@
+export const languages = {
+  en: 'English',
+  es: 'Français',
+}
+
+export const defaultLang = 'es'
+
+export const ui = {
+  en: {
+    'nav.skills': 'Skills',
+    'nav.projects': 'Projects',
+    'nav.about': 'About me',
+    'nav.contact': 'Contact',
+  },
+  es: {
+    'nav.skills': 'Habilidades',
+    'nav.projects': 'Proyectos',
+    'nav.about': 'Sobre mí',
+    'nav.contact': 'Contacto',
+  },
+}
