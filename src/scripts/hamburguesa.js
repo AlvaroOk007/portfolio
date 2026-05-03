0cf84b23fb034f916380c2f7cc4976ab53289a71
Alvaro Bravo <bravoalva19@gmail.com>
2026-05-03 19:55:13 -0300

    Se agregó mediquerys para header  e inicio

---
+++ src/scripts/hamburguesa.js
@@ -0,0 +1,15 @@
+const navBar = document.getElementsByClassName('nav-bar-container')[0]
+const btnHamburguesa = document.getElementById('btn-hamburguesa')
+btnHamburguesa?.addEventListener('click', () => {
+  const estaDesplegado = navBar?.classList.toggle('desplegar')
+  if(estaDesplegado){
+    document.body.style.overflow = 'hidden' 
+  }else{
+    document.body.style.overflow = 'auto' 
+  }
+})
+
+navBar.addEventListener('click',()=>{
+  navBar?.classList.remove('desplegar')
+  document.body.style.overflow = 'auto' 
+})
